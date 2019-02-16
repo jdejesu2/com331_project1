3677ea6ade8fb549e3176e2e7fd579d43ea031a8
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-02-15 22:52:39 -0600

    Ran migrate Compliant form for db schema
      $rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_044403) do
+ActiveRecord::Schema.define(version: 2019_02_16_045038) do
+
+  create_table "complaints", force: :cascade do |t|
+    t.datetime "date"
+    t.string "location"
+    t.string "type"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reporters", force: :cascade do |t|
     t.string "first_name"
