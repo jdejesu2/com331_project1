ae4a26a45856c44adc0f28ba9dd9d84a42eb7257
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-02-15 23:18:05 -0600

    Error, forgot an end,
     PRINTED FROM TERMINAL: SyntaxError: /Users/jessicadejesus/com311/com331
     _project1/db/migrate/20190216050100_create_complaints_reporters_join_ta
     ble.rb:7: syntax error, unexpected end-of-input, expecting end

--- db/migrate/20190216050100_create_complaints_reporters_join_table.rb
+++ db/migrate/20190216050100_create_complaints_reporters_join_table.rb
@@ -3,5 +3,6 @@ class CreateComplaintsReportersJoinTable < ActiveRecord::Migration[5.2]
     create_join_table :complaints, :reporters do |t|
       t.index :complaint_id
       t.index :reporter_id
+    end
   end
 end
