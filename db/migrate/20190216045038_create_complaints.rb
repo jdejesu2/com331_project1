556593e632e1fcaf756e767ff5b7a191427caa8c
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-02-15 22:51:06 -0600

    Created model for complaint form
      $ rails g model Complaint date:datetime
      location:string type:string description:text

---
+++ db/migrate/20190216045038_create_complaints.rb
@@ -0,0 +1,12 @@
+class CreateComplaints < ActiveRecord::Migration[5.2]
+  def change
+    create_table :complaints do |t|
+      t.datetime :date
+      t.string :location
+      t.string :type
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
