7811a7069be4c431c63a405979fa75273c90ec38
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-02-15 23:15:42 -0600

    Migration for the complaint_reporter join table

--- db/migrate/20190216050100_create_complaints_reporters_join_table.rb
+++ db/migrate/20190216050100_create_complaints_reporters_join_table.rb
@@ -1,4 +1,7 @@
 class CreateComplaintsReportersJoinTable < ActiveRecord::Migration[5.2]
   def change
+    create_join_table :complaints, :reporters do |t|
+      t.index :complaint_id
+      t.index :reporter_id
   end
 end
