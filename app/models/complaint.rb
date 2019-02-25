cd76b48aa7a6a02de7c7f64228e30cf1a893ae0c
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-02-25 15:23:57 -0600

    validates for models

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,3 +1,5 @@
 class Complaint < ApplicationRecord
   belongs_to :reporters
+
+  validates :date, :location, :department, :description, presence: true
 end
--- app/models/reporter.rb
+++ app/models/reporter.rb
@@ -1,3 +1,13 @@
 class Reporter < ApplicationRecord
   has_many :complaints
+
+  validates :first_name, :last_name, :a_number, :school_email, presence: true
+
+  validates :a_number, uniqueness: true
+
+  validates :school_email, allow_blank: true,
+  format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    message: 'must be a URL of GIF, JPG, or PNG image.'
+  }
 end
