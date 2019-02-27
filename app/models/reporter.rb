f8b974f30d78e9d92a2bcac05b27be058d02f2e5
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-02-27 16:33:25 -0600

    do not allow blank

--- app/models/reporter.rb
+++ app/models/reporter.rb
@@ -5,10 +5,10 @@ class Reporter < ApplicationRecord
 
   validates :a_number, uniqueness: true
 
-  validates :school_email, allow_blank: true,
+  validates :school_email,
   format: {
     with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
     message: 'must be a URL of GIF, JPG, or PNG image.'
   }
-  
+
 end
