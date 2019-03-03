81d79053645f20ad80e48e70bfe4b2cfabf15274
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-03-03 15:27:44 -0600

    want to remove destory for the user, but confused on how to approach it?

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :complaints
+  #:only => [:index, :new, :create, :edit, :update, :show]
+  #except: [:destroy]
 
   root 'complaints#index'
 end
