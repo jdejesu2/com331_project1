c6fda22166ec2fefe209f7c8f1372308564f50af
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-03-14 16:03:38 -0500

    trying to remove the routes I don't want

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :complaints
-  #:only => [:index, :new, :create, :edit, :update, :show]
-  #except: [:destroy]
+  resources :complaints, only: [:index, :new, :create, :show]
 
   root 'complaints#index'
 end
