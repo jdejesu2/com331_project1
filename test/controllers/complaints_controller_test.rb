9354dd26fcda7d54b410987c65f58dc285dc5391
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-03-15 00:02:20 -0500

    commenting out test im not using since i dont want to edit update or destroy anything

--- test/controllers/complaints_controller_test.rb
+++ test/controllers/complaints_controller_test.rb
@@ -28,21 +28,21 @@ class ComplaintsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_complaint_url(@complaint)
-    assert_response :success
-  end
+  #test "should get edit" do
+    #get edit_complaint_url(@complaint)
+    #assert_response :success
+  #end
 
-  test "should update complaint" do
-    patch complaint_url(@complaint), params: { complaint: { date: @complaint.date, location: @complaint.location, department: @complaint.department, description: @complaint.description, person_number: @complaint.reporter.a_number } }
-    assert_redirected_to complaint_url(@complaint)
-  end
+  #test "should update complaint" do
+    #patch complaint_url(@complaint), params: { complaint: { date: @complaint.date, location: @complaint.location, department: @complaint.department, description: @complaint.description, person_number: @complaint.reporter.a_number } }
+    #assert_redirected_to complaint_url(@complaint)
+  #end
 
-  test "should destroy complaint" do
-    assert_difference('Complaint.count', -1) do
-      delete complaint_url(@complaint)
-    end
+  #test "should destroy complaint" do
+    #assert_difference('Complaint.count', -1) do
+      #delete complaint_url(@complaint)
+    #end
 
-    assert_redirected_to complaints_url
-  end
+    #assert_redirected_to complaints_url
+  #end
 end
